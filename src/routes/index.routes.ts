019844f36b68ce915f2dfb945308647e45c5cf81
gsolorzano <g.solorzano@estudiantec.cr>
2020-03-09 22:31:35 -0600

    Se crea el CRUD para investigation_unit

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import Network from './network.routes';
 import Academic_Unit from './academic_unit.routes';
+import Investigation_Unit from './investigation_unit.routes';
 import Center from './center.routes'
 export const addRoutes = (app: any) => {
     app.use(Network);
     app.use(Center);
     app.use(Academic_Unit);
+    app.use(Investigation_Unit);
 }
