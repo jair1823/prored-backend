40dded850a3d52f32a79430859c956741b1cf930
gsolorzano <g.solorzano@estudiantec.cr>
2020-03-14 12:53:08 -0600

    Se estandariza el cursor

--- src/database/sps.sql
+++ src/database/sps.sql
@@ -1,46 +1,8 @@
 ----------------------------------------------------------------------------------------------------------------- 
--- Get Students Manual
-
--- DROP FUNCTION getstudents();
-
-CREATE OR REPLACE FUNCTION getstudents () 
-   RETURNS TABLE (
-      dni VARCHAR,
-      name VARCHAR,
-	  lastname1 VARCHAR,
-	  lastname2 VARCHAR,
-	  born_dates DATE,
-   	  id_district INT,
-	  district VARCHAR,
-	  campus_code VARCHAR,
-	  campus VARCHAR,
-	  marital_status marital_status,
-	  profile profile,
-	  address TEXT,
-	  nationality nationality
-   )
-AS $$
-BEGIN
-   RETURN QUERY select 
-p.dni, p.name, p.lastname1, p.lastname2,
-p.born_dates, d.id_district ,d.name as district, 
-c.campus_code, c.name as campus,
-s.marital_status, s.profile, s.address, s.nationality
-
-from public.person p
-inner join public.student s on s.dni = p.dni
-inner join public.district d on d.id_district = s.id_district
-inner join public.campus c on c.campus_code = s.campus_code
-where p.status = true;
-END; $$ 
- 
-LANGUAGE 'plpgsql';
-
------------------------------------------------------------------------------------------------------------------ 
 
 -- Get Students con Cursor
 
-CREATE OR REPLACE FUNCTION getstudents2(ref refcursor) RETURNS refcursor AS $$
+CREATE OR REPLACE FUNCTION getstudents(ref refcursor) RETURNS refcursor AS $$
 BEGIN
   OPEN ref FOR select 
 p.dni, p.name, p.lastname1, p.lastname2,
