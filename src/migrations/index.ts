8e4c9f0c3bf1e9358b23a3ee5bb213b7eea31197
jair1823 <cordero1823.cr@gmail.com>
2020-03-14 21:48:21 -0600

    Sps student add to migrate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,14 +2,18 @@ import { pool } from '../database/connection';
 import {readFileSync} from 'fs';
 
 const dropAll = readFileSync('src/database/drops.sql').toString();
+const dropSpsStudent = readFileSync('src/database/spsStudentDrops.sql').toString();
 const tables = readFileSync('src/database/tables.sql').toString();
 const spsSQL = readFileSync('src/database/sps.sql').toString();
+const spsStuden = readFileSync('src/database/spsStudent.sql').toString();
 
 const drops = async () => {
     try {
         await pool.query(dropAll);
+        await pool.query(dropSpsStudent);
         await pool.query(tables);
         await pool.query(spsSQL);
+        await pool.query(spsStuden);
         return;
     } catch (error) {
         console.log(error);
