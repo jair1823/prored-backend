a081e6952c809694bd2b00cca027f57a1bf1a8b3
Gabriel Solórzano <g.solorzano@estudiantec.cr>
2020-05-19 10:19:49 -0600

    Se agrega Morgan para develop

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { addRoutes } from './routes/index.routes';
+const morgan = require('morgan');
 const app = express();
 
 //middlwares
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
