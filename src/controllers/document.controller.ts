c16ef1013b4d88d15c7aa0c58e1e93bc9cd1e6ac
Gabriel Solórzano <g.solorzano@estudiantec.cr>
2020-05-30 10:55:01 -0600

    Se agrega la funcionalidad de agregar un aval

--- src/controllers/document.controller.ts
+++ src/controllers/document.controller.ts
@@ -18,8 +18,8 @@ export class DocumentController {
         const insert = `SELECT createprojectform($1,$2,$3,$4);`;
         try {
             const url = `${req.body.tabla}/${req.file.filename}`;
-            const today = new Date().toISOString().slice(0, 10)
-            const values = [req.body.id_project,today, req.file.filename, url];
+            const today = new Date().toISOString().slice(0, 10);
+            const values = [req.body.id_project, today, req.file.filename, url];
             await Queries.simpleTransaction(insert, values, client);
             return res.status(200).json(
                 {
@@ -39,7 +39,7 @@ export class DocumentController {
     /**
      * Delete a Project Form from a project.
      * path: /project_form/:id
-     * method: post
+     * method: delete
      */
 
     async deleteProjectForm(req: Request, res: Response): Promise<Response> {
@@ -51,10 +51,9 @@ export class DocumentController {
             const id = [req.params.id];
             await Queries.begin(client);
             const response = await Queries.simpleSelectWithParameterContinous(query, id, fetch, client);
-            let message = "empty"
+            let message = "empty";
             let resultado = response.rows[0];
             if (resultado != undefined) {
-                console.log("Entre")
                 const p = resultado.file_path;
                 let fullPath = path.join(__dirname + '../../..' + '/public/' + p);
                 fs.unlinkSync(fullPath);
@@ -93,6 +92,122 @@ export class DocumentController {
                     msg: "empty"
                 });
             }
+            return res.json(rows);
+        } catch (error) {
+            await Queries.simpleError(client, error);
+            return res.status(500).json({
+                msg: 'Internal Server Error'
+            });
+        }
+    }
+
+    /**
+     * Create a Endorsement from a project.
+     * path: /endorsement
+     * method: post
+     */
+
+    async insertEndorsement(req: Request, res: Response): Promise<Response> {
+        const client: PoolClient = await pool.connect();
+        const insert = `SELECT createendorsement($1,$2,$3,$4);`;
+        try {
+            const url = `${req.body.tabla}/${req.file.filename}`;
+            const values = [req.body.id_project, req.body.endorsement_type, req.file.filename, url];
+            await Queries.simpleTransaction(insert, values, client);
+            return res.status(200).json(
+                {
+                    msg: 'Endorsement inserted'
+                }
+            );
+        } catch (error) {
+
+            await Queries.simpleError(client, error);
+
+            return res.status(500).json({
+                msg: 'Internal Server Error'
+            });
+        }
+    }
+
+    /**
+     * Delete a Endorsement from a project.
+     * path: /endorsement/:id
+     * method: delete
+     */
+
+    async deleteEndorsement(req: Request, res: Response): Promise<Response> {
+        const client: PoolClient = await pool.connect();
+        const deleteD = `SELECT deleteendorsement($1);`;
+        const query = `SELECT getendorsement($1,'endCursor');`;
+        const fetch = `FETCH ALL IN "endCursor";`;
+        try {
+            const id = [req.params.id];
+            await Queries.begin(client);
+            const response = await Queries.simpleSelectWithParameterContinous(query, id, fetch, client);
+            let message = "empty";
+            let resultado = response.rows[0];
+            if (resultado != undefined) {
+                const p = resultado.file_path;
+                let fullPath = path.join(__dirname + '../../..' + '/public/' + p);
+                fs.unlinkSync(fullPath);
+                await Queries.simpleTransaction(deleteD, id, client);
+                message = "Endorsement deleted";
+            }
+            return res.status(200).json(
+                {
+                    msg: message
+                }
+            );
+        } catch (error) {
+            await Queries.simpleError(client, error);
+            return res.status(500).json({
+                msg: 'Internal Server Error'
+            });
+        }
+    }
+
+    /**
+     * Get Endorsement.
+     * path: /endorsement/:id
+     * method: get
+     */
+
+    async getEndorsement(req: Request, res: Response): Promise<Response> {
+        const query = `select getendorsement($1,'endCursor');`;
+        const fetch = `FETCH ALL IN "endCursor";`;
+        const client = await pool.connect();
+        try {
+            const dni = [req.params.id];
+            const response = await Queries.simpleSelectWithParameter(query, dni, fetch, client);
+            const rows = response.rows[0];
+            if (rows === undefined) {
+                return res.json({
+                    msg: "empty"
+                });
+            }
+            return res.json(rows);
+        } catch (error) {
+            await Queries.simpleError(client, error);
+            return res.status(500).json({
+                msg: 'Internal Server Error'
+            });
+        }
+    }
+
+    /**
+     * Get Endorsements from project.
+     * path: /endorsement/project/:id
+     * method: get
+     */
+
+    async getEndorsementsProject(req: Request, res: Response): Promise<Response> {
+        const query = `select getendorsementsproject($1,'endCursor');`;
+        const fetch = `FETCH ALL IN "endCursor";`;
+        const client = await pool.connect();
+        try {
+            const dni = [req.params.id];
+            const response = await Queries.simpleSelectWithParameter(query, dni, fetch, client);
+            const rows = response.rows;
             return res.json(rows);
         } catch (error) {
             await Queries.simpleError(client, error);
--- src/database/Drops/DocumentDrops.sql
+++ src/database/Drops/DocumentDrops.sql
@@ -1,3 +1,7 @@
 DROP FUNCTION IF EXISTS createprojectform;
 DROP FUNCTION IF EXISTS deleteprojectform;
 DROP FUNCTION IF EXISTS getprojectform;
+DROP FUNCTION IF EXISTS createendorsement;
+DROP FUNCTION IF EXISTS deleteendorsement;
+DROP FUNCTION IF EXISTS getendorsement;
+DROP FUNCTION IF EXISTS getendorsementsproject;
