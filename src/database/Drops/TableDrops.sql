559b5bc647d91ed1711a51cfeb84ac9f777b9afa
Nacho <nacho4real@estudiantec.cr>
2020-06-19 14:42:03 -0600

    Se crea el drop de financial item table

--- src/database/Drops/TableDrops.sql
+++ src/database/Drops/TableDrops.sql
@@ -32,6 +32,7 @@ DROP TABLE IF EXISTS public.province;
 DROP TABLE IF EXISTS public.person;
 DROP TABLE IF EXISTS public.budget_unit;
 DROP TABLE IF EXISTS public.budget_sub_unit;
+DROP TABLE IF EXISTS public.financial_item;
 DROP TYPE IF EXISTS public.role;
 DROP TYPE IF EXISTS public.degree;
 DROP TYPE IF EXISTS public.network_type;
